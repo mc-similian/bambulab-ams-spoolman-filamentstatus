efa142b553c56525217ffde27a3c3badc5518553
Rdiger-36 <mail@ratka.net>
2025-02-19 19:06:09 +0100

    Update version

--- backend.js
+++ backend.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url';
 // loading .env
 config();
 
-const version = "1.0.2";
+const version = "1.0.3";
 const app = express();
 const PORT = 4000; // Port for backend --> also used by frontend for Web UI
 
